c969af0ab5dde51d6580fd469cc62daed6e59e94
Gavin Mc Gregor <gavinm@agilebridge.co.za>
2022-04-03 13:47:07 +0200

    udpate to .net 6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Core.Entities;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +7,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController: ControllerBase
+    public class ProductsController : ControllerBase
     {
         private readonly StoreContext _context;
         public ProductsController(StoreContext context)
@@ -18,14 +16,13 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(){
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
             var products = await _context.Products.ToListAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id){
-            return await _context.Products.FindAsync(id);
-        }
+        public async Task<ActionResult<Product>> GetProduct(int id) => await _context.Products.FindAsync(id);
     }
 }
